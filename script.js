f96354923730045a6a4964031f7c5450422d6836
Henry <aiapplicator341@gmail.com>
2025-02-25 19:07:59 -0500

    Audio banner, def volume 30% and fix, clean vars

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const volumeSlider = document.getElementById('volumeSlider');
 const playPauseButton = document.getElementById('playPauseButton'); // Select play/pause button
 
 let isMusicPlaying = false; // Variable to track music state (initially paused)
+backgroundMusic.volume = volumeSlider.value;
+
 
 newGameButton.addEventListener('click', function() {
     gameContainer.innerHTML = '<h2>Starting a New Game!</h2><p>Game content will load here...</p>';
@@ -17,7 +19,6 @@ playPauseButton.addEventListener('click', function() {
         isMusicPlaying = false;
         playPauseButton.textContent = "Play"; // Change button text to "Play" when paused
     } else {
-        backgroundMusic.volume = volumeSlider.value;
         backgroundMusic.play();
         isMusicPlaying = true;
         playPauseButton.textContent = "Pause"; // Change button text to "Pause" when playing
